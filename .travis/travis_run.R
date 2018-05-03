3d1a81c130cfadd5492ea775d0c5a90f074bfb3e
Jonathan Sidi <yonicd@gmail.com>
2018-05-03 14:38:18 -0400

    change test

--- .travis/travis_run.R
+++ .travis/travis_run.R
@@ -3,4 +3,6 @@ print(getwd())
 install.packages(c('testthat','covr'))
 devtools::install_github('yonicd/covrpage')
 
+row.names(installed.packages())
+
 covrpage::covrpage(pkg = '.', preview = FALSE)

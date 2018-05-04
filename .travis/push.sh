1f8b24e7ff7d4a32cb363732b7fe14ab0fa042c9
Jonathan Sidi <yonicd@gmail.com>
2018-05-04 09:25:35 -0400

    change pat envir name

--- .travis/push.sh
+++ .travis/push.sh
@@ -13,7 +13,7 @@
   git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
   
-  git remote add deploy https://yonicd:${GITHUB_PAT}@github.com/yonicd/rtravis.git
+  git remote add deploy https://yonicd:${GH_PAT}@github.com/yonicd/rtravis.git
   git push -f deploy test -v
   
   else

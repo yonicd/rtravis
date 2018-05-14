95b75b99a85cbb959a1fa6ee3829efb2f5199f41
Jonathan Sidi <yonicd@gmail.com>
2018-05-14 10:41:35 -0400

    add travis_encrypt [skip ci]

--- R/fun.R
+++ R/fun.R
@@ -15,3 +15,22 @@
 fun <- function(x,y){
   x+y
 }
+
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param x PARAM_DESCRIPTION
+#' @param y PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname travis_encrypt
+#' @export 
+
+travis_encrypt <- function(x,y){
+  x+y
+}
